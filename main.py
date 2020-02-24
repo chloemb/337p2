5388ad576f6b149229199c72af1b5ddbd66a871f
73Nixon73 <36314432+73Nixon73@users.noreply.github.com>
2020-02-24 06:12:42 -0600

    Ugly solution to category parsing

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ recipe = retrieve_dict()
 #Returns a dict. "Name", "Ingredients" and "Procedure" return appropriate information in a list (except name. Name is not in a list)
 
 parsed_ingredients = parse_ingredients(recipe['Ingredients'])
+
+deletelist = []
+for ingredient,details in parsed_ingredients.items():
+    if details==dict():
+        deletelist.append(ingredient)
+for deletething in deletelist:
+    parsed_ingredients.pop(deletething)
 print("\nRETRIEVED:", recipe, '\n')
 print("PARSED INGREDIENTS", parsed_ingredients, '\n')
