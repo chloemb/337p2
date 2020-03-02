d665da4d0bbb18b83b238809f82a9cdc99554207
73Nixon73 <36314432+73Nixon73@users.noreply.github.com>
2020-03-01 18:38:13 -0600

    Adds vegetarian, improves ingredient parsing

--- vegetarianify.py
+++ vegetarianify.py
@@ -1,6 +1,16 @@
 from word_banks import not_vegetarian
+import sys
 
 def vegetarian(ingredients,recipe,steps):
+    
+    newsteps = {}
+    for step,thing in steps.items():
+        for why,actually in thing.items():
+            for key,contents in actually.items():
+                newsteps[key]=contents
+
+    steps=newsteps
+
     #check to see if anything needs to be done
     #if it does, store non-vegetarian things in a list
     vegetarian = True
@@ -14,34 +24,79 @@ def vegetarian(ingredients,recipe,steps):
     if vegetarian:
         return ingredients,recipe,steps
 
+    print(non_veg)
+    for problem in non_veg:
+        resolved = False
+        #deal w specific things (spices, bouillon)
+        for edge, replace in edgefoods.items():
+            if edge in problem:
+                resolved=True
+                ingredients[replace] = ingredients[problem]
+                for stepverb, stepstuff in steps.items():
+                    if problem in stepstuff['Ingredients']:
+                        stepstuff['Ingredients'].append(replace)
+                        stepstuff['Ingredients'].remove(problem)
+                ingredients.pop(problem)
+        if not resolved:
+            adapt(ingredients,steps,problem, recipe)
+            
+
+    return ingredients, steps
+
+        
+
+        
+    
+             
+def decide_replace(problem,steps,recipe):
     #try to figure protein recommendation based on type of food
     foodtype = ""
-    for classifier in [main,stewey,greenmix]:
-        for foodgroup in classifier['title']:
-            if foodgroup in recipe['title']:
-                foodtype = classifier['ret']
-    if foodtype == "":
-        #try to figure protein recommendation based on things done to ingredients here
-        #fancier version will add probabilities or something
+    #try to figure protein recommendation based on things done to ingredients here
+    #fancier version will add probabilities or something
+    for stepv,stepstuff in steps.items():
+        if problem not in stepstuff['Ingredients']:
+            continue
         for classifier in [main,stewey,greenmix]:
-            for verb in classifier['steps']:
-                if verb in steps:
-                    foodtype = classifier['ret']
-    #we can default to hard tofu. its the stereotype for a reason
+            if any(verb in stepstuff['Related_Verbs'] for verb in classifier['steps']):
+                return classifier['ret']
     if foodtype == "":
-        foodtype = 'generic'
-
+        for classifier in [main,stewey,greenmix]:
+            for foodgroup in classifier['title']:
+                if foodgroup in recipe['Name']:
+                    return classifier['ret']
+    #we can default to hard tofu. its the stereotype for a reason
+    return 'hard tofu'
 
     #main replacement loop
     #first we deal with edge foods
     #Then we replace standard uses of the things
-    replacements = {}
-    for problem in non_veg:
-        resolved = False
-    
-             
 
-    
+
+
+def adapt(ingredients, steps,problem,recipe):
+    new = decide_replace(problem,steps,recipe)
+    #if its already there, all we have to do is add seitan and switch mentions of problem ingredient to seitan.
+    try:
+        ingredients[new]['quantity'] += ingredients[new]['quantity']
+        for stepverb, stepstuff in steps.items():
+            if problem in stepstuff['Ingredients']:
+                stepstuff['Ingredients'].append(new)
+                stepstuff['Ingredients'].remove(problem)
+        ingredients.pop(problem)
+        return
+    #else for ingredients we make it take the quantity and details of the original
+    #for steps we append basic prep for the ingredient in question
+    except:
+        ingredients[new] = ingredients[problem]
+        for stepverb, stepstuff in steps.items():
+            if problem in stepstuff['Ingredients']:
+                stepstuff['Ingredients'].append(new)
+                stepstuff['Ingredients'].remove(problem)
+            #also add prep for the ingredient??
+        ingredients.pop(problem)
+        
+
+
 
     
 
@@ -49,13 +104,13 @@ def vegetarian(ingredients,recipe,steps):
 #aim is to return the same dict as before, replace title, ingredients and steps
 
 #for small bits of flavor and dishes where this is supposed to be the main, seitan makes the most sense
-main = {'title':['taco','burrito','rice bowl','roast'],'steps':['stir-fry','roast','grill','dress'],'ret':'main'}
+main = {'title':['taco','burrito','rice bowl','roast'],'steps':['stir-fry','roast','grill','dress'],'ret':'seitan'}
 
 #if we're sinking it into the flavor of something, tofu is the only one that makes sense
-stewey = {'title':['soup','curry','stew'],'steps':['marinate','steep','steam','brew'],'ret':'stew'}
+stewey = {'title':['soup','curry','stew'],'steps':['marinate','steep','steam','brew','roast'],'ret':'soft tofu'}
 
 #Basically no salads will look weird substituting w tempeh
-greenmix = {'title':['salad','greens'],'steps':['toss'],'ret':'salad'}
+greenmix = {'title':['salad','greens'],'steps':['toss'],'ret':'tempeh'}
 
 #add more to these?
-edgefoods = {'bouillon':'vegetable bouillon','broth':'vegetable broth','fat':'lard'}
+edgefoods = {'bouillon':'vegetable bouillon','broth':'vegetable broth','fat':'lard','seasoning':'allspice'}
