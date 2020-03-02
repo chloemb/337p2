4fce1e8806f52069ac67cc4e7f98daf2d9d13318
Jack Warshaw <jackwarshaw10@gmail.com>
2020-03-01 19:56:33 -0600

    Chicken solved

--- step_parser.py
+++ step_parser.py
@@ -1,6 +1,7 @@
 import fractions
 import re
 import nltk
+from word_banks import toollist, measurement_bank
 
 # problems: words that are shortened in steps (oil for olive oil),
 # mischaracterizing specific words (verbs as nouns, nouns as other things),
@@ -16,10 +17,10 @@ def parse_steps(steps, ingredients):
         sentences = step.split('.')
         #loop through the sentences in a step
         for sentence in sentences:
-            sentence = sentence + "."
+            sentence_mod = sentence + " ."
             sentence_dict = {}
             #tag the words in a sentence with a POS tagger
-            tokens = nltk.word_tokenize(sentence.lower())
+            tokens = nltk.word_tokenize(sentence_mod.lower())
             tagged = nltk.pos_tag(tokens)
             #have a bool that marks when you should be storing things within an associated verb
             start_verb = False
@@ -44,7 +45,6 @@ def parse_steps(steps, ingredients):
                         stored_info.update({"Times": timing})
                         stored_info.update({"Related_Verbs": verb_list})
                         sentence_dict[''.join(most_recent_adverb + most_recent_verb)] = stored_info
-                        print(sentence_dict)
                         most_recent_adverb = ''
                         start_verb = False
                         part_of_time = False
@@ -62,13 +62,35 @@ def parse_steps(steps, ingredients):
                         verb_list = []
                         for ingredient in ingredients:
                             if ingredient not in ingredient_list:
-                                if ingredient in sentence:
+                                if re.search(" " + ingredient + " ", sentence_mod):
                                     ingredient_list.append(ingredient)
+                                    quantity = []
+                                    for word in sentence_mod:
+                                        print(word)
+                                        if word in measurement_bank:
+                                            qunatity.append(word)
                             if ingredient not in ingredient_list:
                                 if len(ingredient.split()) > 1:
-                                    holder_ingredient = ingredient.split()[1]
-                                    if holder_ingredient in sentence:
-                                        ingredient_list.append(ingredient)
+                                    j = len(ingredient.split())
+                                    while j > 0:
+                                        print(j)
+                                        i = 0
+                                        while i < j:
+                                            holder = ingredient.split()[i:j]
+                                            print(holder)
+                                            holder_ingredient = ''
+                                            for k in holder:
+                                                holder_ingredient = holder_ingredient + k + ' '
+                                            print(holder_ingredient)
+                                            if re.search(" " + holder_ingredient, sentence_mod):
+                                                ingredient_list.append(ingredient)
+                                                print("break1")
+                                                break
+                                            i = i + 1
+                                        if ingredient in ingredient_list:
+                                            print("break2")
+                                            break
+                                        j = j - 1
                         stored_info.update({"Ingredients": ingredient_list})
                     if pairs[1] == 'RB':
                         most_recent_adverb = pairs[0] + " "
@@ -77,4 +99,5 @@ def parse_steps(steps, ingredients):
             if sentence == '.':
                 del step_parsed['.']
         steps_by_number[''.join(step)] = step_parsed
+    print(steps_by_number)
     return steps_by_number
